b9357111e872f943b3bbc431b4633b15d271f38c
shreyanshanand <s.anand@queensu.ca>
2018-11-24 00:14:05 -0500

    fixing up the names and adding a sick and sexy class for signup

--- app/src/main/java/ca/queensu/toft/naturally/SignUp.java
+++ app/src/main/java/ca/queensu/toft/naturally/SignUp.java
@@ -3,6 +3,14 @@ package ca.queensu.toft.naturally;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class SignUp extends AppCompatActivity {
 
     @Override
