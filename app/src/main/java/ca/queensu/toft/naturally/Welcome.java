abd32adb97ecd8c44ae27382643be9b2092893d6
Christopher Goel <christopher.goel@gmail.com>
2018-11-23 23:34:36 -0500

    More UX trash

---
+++ app/src/main/java/ca/queensu/toft/naturally/Welcome.java
@@ -0,0 +1,34 @@
+package ca.queensu.toft.naturally;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        Button loginButton;
+        Button signupButton;
+        loginButton = (Button)findViewById(R.id.login_button);
+        signupButton = (Button)findViewById(R.id.sign_up_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Login.class);
+                startActivity(i);
+            }
+        });
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(i);
+            }
+        });
+    }
+}
