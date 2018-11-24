46255c2632609939bcd7415320be13f0153f1d05
Christopher Goel <christopher.goel@gmail.com>
2018-11-24 01:55:31 -0500

    Flashy frontpage trash

--- app/src/main/java/ca/queensu/toft/naturally/Welcome.java
+++ app/src/main/java/ca/queensu/toft/naturally/Welcome.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Welcome extends AppCompatActivity {
 
@@ -17,8 +18,10 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
         Button loginButton;
         Button signupButton;
+        ImageView treeView;
         loginButton = (Button)findViewById(R.id.login_button);
         signupButton = (Button)findViewById(R.id.sign_up_button);
+        treeView = (ImageView)findViewById(R.id.tree);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,14 +37,39 @@ public class Welcome extends AppCompatActivity {
             }
         });
 
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 //            Path path = new Path();
-//            path.arcTo(0f, 0f, 1000f, 1000f, 270f, -180f, true);
-//            ObjectAnimator animator = ObjectAnimator.ofFloat(loginButton, View.X, View.Y, path);
-//            animator.setDuration(2000);
-//            animator.start();
-//        } else {
-//            // Create animator without using curved path
-//        }
+//            path.arcTo(0f, 1000f, 0f, 0f, 0f, 0f, true);
+            float treeX = 10f;
+            float treeY = 1000f;
+            treeView.setY(treeY);
+            ObjectAnimator animator = ObjectAnimator.ofFloat(treeView, "translationY", 8f);
+            animator.setDuration(2000);
+            animator.start();
+            treeView = (ImageView) findViewById(R.id.tree2);
+            treeView.setY(treeY);
+            treeX += 330;
+            treeView.setX(treeX);
+            animator = ObjectAnimator.ofFloat(treeView, "translationY", 8f);
+            animator.setStartDelay(1000);
+            animator.setDuration(2000);
+            animator.start();
+            treeView = (ImageView) findViewById(R.id.tree3);
+            treeView.setY(treeY);
+            treeX += 350;
+            treeView.setX(treeX);
+            animator = ObjectAnimator.ofFloat(treeView, "translationY", 8f);
+            animator.setStartDelay(2000);
+            animator.setDuration(2000);
+            animator.start();
+            ImageView sunView = (ImageView) findViewById(R.id.sun);
+            sunView.setY(-2000f);
+            sunView.setX(0);
+            animator = ObjectAnimator.ofFloat(sunView, "translationY", -1000f);
+            animator.setDuration(2000);
+            animator.start();
+        } else {
+            // Create animator without using curved path
+        }
     }
 }
