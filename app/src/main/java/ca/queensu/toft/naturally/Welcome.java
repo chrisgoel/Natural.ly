d756a85eebfd0ab38be1305389d3c532c9e89a72
Christopher Goel <christopher.goel@gmail.com>
2018-11-24 00:10:35 -0500

    Connecting pages to login and signup buttons

--- app/src/main/java/ca/queensu/toft/naturally/Welcome.java
+++ app/src/main/java/ca/queensu/toft/naturally/Welcome.java
@@ -1,6 +1,9 @@
 package ca.queensu.toft.naturally;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.Path;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,5 +33,15 @@ public class Welcome extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            Path path = new Path();
+//            path.arcTo(0f, 0f, 1000f, 1000f, 270f, -180f, true);
+//            ObjectAnimator animator = ObjectAnimator.ofFloat(loginButton, View.X, View.Y, path);
+//            animator.setDuration(2000);
+//            animator.start();
+//        } else {
+//            // Create animator without using curved path
+//        }
     }
 }
